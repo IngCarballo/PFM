bc92ef0a626378a4534b2e53ed79fd3231abd3c0
nicolasCarballo <nicolas.carballo@um.edu.ar>
2025-04-07 22:27:23 -0300

    Update views

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Movement, Category
+from .serializers import MovementSerializer, CategorySerializer
 
-# Create your views here.
+class MovementViewSet(viewsets.ModelViewSet):
+    """
+    Vista para manejar operaciones CRUD sobre movimientos.
+    """
+    serializer_class = MovementSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Solo usuarios logueados pueden acceder
+
+    def get_queryset(self):
+        # Filtramos los movimientos para que el usuario solo vea los suyos
+        return Movement.objects.filter(user=self.request.user).order_by('-date')
+
+    def perform_create(self, serializer):
+        # Asignamos el usuario automáticamente cuando se crea un movimiento
+        serializer.save(user=self.request.user)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    Vista para manejar operaciones CRUD sobre categorías.
+    """
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Si las categorías son compartidas por todos los usuarios, devolvemos todas.
+        return Category.objects.all()
