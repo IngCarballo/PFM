4df7e1c5a0daa16616ea11a1d42c160271206ccc
nicolasCarballo <nicolas.carballo@um.edu.ar>
2025-04-07 21:56:59 -0300

    Update settings.py
    CREATE DATABASE pfm_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
    CREATE USER 'pfm_user'@'localhost' IDENTIFIED BY '8004-85c7';
    GRANT ALL PRIVILEGES ON pfm_db.* TO 'pfm_user'@'localhost';
    FLUSH PRIVILEGES;

--- finance_manager/settings.py
+++ finance_manager/settings.py
@@ -74,8 +74,16 @@ WSGI_APPLICATION = 'finance_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pfm_db',
+        'USER': 'pfm_user',
+        'PASSWORD': '8004-85c7',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
