6c530d9f246996b2bd5726ea3a098d5498cb1ff8
nicolasCarballo <nicolas.carballo@um.edu.ar>
2025-04-08 14:14:15 -0300

    Update urls.py

--- finance_manager/urls.py
+++ finance_manager/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include  # `include` permite traer las rutas definidas en otras apps
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-# Lista de rutas principales del proyecto
 urlpatterns = [
     path('admin/', admin.site.urls),                        # Ruta para acceder al panel de administración de Django
     path('api/', include('transactions.urls')),             # Incluye todas las URLs de la app 'transactions' bajo el prefijo /api/
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/accounts/', include('accounts.urls')),  # tu nueva app (si usás más vistas)
 ]
